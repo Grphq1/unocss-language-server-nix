57baa63492b4b19cb67d7e988a9055d30ad89508
lorem10 <momenei.alireza1382.15@gmail.com>
2026-08-02 21:53:46 +0330

    init project

---
+++ README.md
@@ -0,0 +1,9 @@
+# unocss-language-server-nix
+
+Unofficial Nix flake for [UnoCSS Language Server](https://github.com/unocss/unocss).
+
+## Usage
+
+```bash
+nix run github:grphq1/unocss-language-server-nix
+```
---
+++ flake.nix
@@ -0,0 +1,72 @@
+{
+  description = "UnoCSS Language Server";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.default = pkgs.stdenv.mkDerivation (finalAttrs: {
+          pname = "unocss-language-server";
+          version = "66.7.5";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "unocss";
+            repo = "unocss";
+            rev = "v${finalAttrs.version}";
+            hash = "sha256-sm1Grc8qcQ0tDeAHV92l5WiIaHjAHfzTUSKLp1zvJAs=";
+          };
+
+          env.CI = "true";
+
+          pnpmDeps = pkgs.fetchPnpmDeps {
+            inherit (finalAttrs) pname version src;
+            hash = "sha256-kj9eioTstD+ahDjgup1RrGUxgqjTpcii3smj8eOxox8=";
+            fetcherVersion = 4;
+          };
+
+          nativeBuildInputs = with pkgs; [
+            nodejs
+            pnpm
+            pnpmConfigHook
+            makeBinaryWrapper
+            typescript
+          ];
+
+          pnpmInstallFlags = ["--ignore-scripts"];
+
+          buildPhase = ''
+            runHook preBuild
+            pnpm --filter "@unocss/language-server..." --filter "./packages-presets/*" run build
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out/lib/unocss-language-server
+
+            cp -r packages-integrations/language-server/dist $out/lib/unocss-language-server/
+            cp -r packages-integrations/language-server/bin  $out/lib/unocss-language-server/
+            cp packages-integrations/language-server/package.json $out/lib/unocss-language-server/
+
+            makeWrapper ${pkgs.lib.getExe pkgs.nodejs} \
+              $out/bin/unocss-language-server \
+              --inherit-argv0 \
+              --add-flags $out/lib/unocss-language-server/bin/unocss-language-server.js
+
+            runHook postInstall
+          '';
+        });
+      }
+    );
+}
